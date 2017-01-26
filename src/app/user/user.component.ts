57330fa606b12d6db300e6b0b02fba2c5072b100
Geraldine <geraldine@g-angle.co.jp>
2017-01-26 13:53:26 +0800

    Add imperative routing

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-component',
   template: `
       <h1>User Component</h1>
-      <a [routerLink]="['../']">Home</a>
+      <a [routerLink]="['/user']">User</a>
+      <button (click)="onNavigate()">Go Home</button>
     `
   })
 export class UserComponent {
+  constructor(private router: Router) {}
+
+  onNavigate(){
+    this.router.navigate(['/']);
+  }
 }
