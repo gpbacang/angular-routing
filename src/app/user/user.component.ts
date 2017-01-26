98742995000bfe42167f137e27048961bb3f4269
Geraldine <geraldine@g-angle.co.jp>
2017-01-26 13:36:08 +0800

    Add navigation paths

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-component',
   template: `
       <h1>User Component</h1>
+      <a [routerLink]="['../']">Home</a>
     `
-})
+  })
 export class UserComponent {
 }
