bc610869402644fa1b23aecd7daf220660d58853
Geraldine <geraldine@g-angle.co.jp>
2017-01-26 15:16:37 +0800

    Add fetching route params

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from "rxjs/Rx";
 
 @Component({
   selector: 'app-user-component',
   template: `
       <h1>User Component</h1>
-      <a [routerLink]="['/user']">User</a>
       <button (click)="onNavigate()">Go Home</button>
+      <hr>
+      {{id}}
     `
   })
-export class UserComponent {
-  constructor(private router: Router) {}
+export class UserComponent implements OnDestroy {
+  private subscription: Subscription;
+
+  id: string;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.subscription = activatedRoute.params.subscribe(
+      (param: any) => this.id = param['id']
+    );
+  }
 
   onNavigate(){
     this.router.navigate(['/']);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
